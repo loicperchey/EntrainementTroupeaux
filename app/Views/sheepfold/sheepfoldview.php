5aa1d655da908287eaa595cc98227acb512156a7
Admin <perchey.loic@gmail.com>
2022-05-07 21:12:44 +0200

    correction for php

--- app/Views/sheepfold/sheepfoldview.php
+++ app/Views/sheepfold/sheepfoldview.php
@@ -12,13 +12,17 @@
             <?php foreach($fields as $field){ ?>
                 <td><?= esc($sheepfold_item[$field]) ?></td>
             <?php } ?>
-        </tr>
+           <!-- <td>
+                <a href="<//?= base_url('shopupdate'.$shop.['idShop']); ?>">Modifier</a>
+                <a href="<//?= base_url('shopremove'.$shop.['idShop']); ?>">Supprimer</a>
+            </td>
+        </tr>-->
        </table>
 
     <?php } ?>
 
 <?php } else { ?>
 
-    <h3>Aucune bergerie</h3>
+    <h3>Aucune bergerie présente</h3>
 
 <?php } ?>
